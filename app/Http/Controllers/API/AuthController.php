4f7488bffa3920d7aed0a449a08f582aad86238e
amrinaas96 <amrinaas96@gmail.com>
2021-09-09 13:22:16 +0700

    Add login adn register API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Traits\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+    use JsonResponse;
+
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'confirm_password' => 'required|same:password',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['name'] =  $user->name;
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+
+        return $this->sendResponse('User register successfully.', $success);
+    }
+   
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['name'] =  $user->name;
+            $success['token'] =  $user->createToken('MyApp')->accessToken; 
+
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,66 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($result, $message)
+    {
+    	$response = [
+            'success' => true,
+            'message' => $message,
+            'data'    => $result,
+        ];
+
+
+        return response()->json($response, 200);
+    }
+
+    // public function sendResponse($msg, $data, $code = 200, $version = '1.0')
+    // public function sendResponse($msg, $data)
+    // {
+    //     return response()->json([
+    //         'success' => true,
+    //         'message' => $msg,
+    //         'data' => $data,
+    //         'code' => $code,
+    //         'version' => $version
+    //     ], $code);
+
+    //     $response = [
+    //         'success' => true,
+    //         'message' => $msg,
+    //         'data' => $data,
+    //         'code' => $code,
+    //         'version' => $version
+    //     ];
+
+    //     return response()->json($response, 200);
+    // }
+
+
+    // /**
+    //  * return error response.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+
+        return response()->json($response, $code);
+    }
+
 }
